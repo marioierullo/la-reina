7ebc0a57654c804bc80066bd1256b219aec82a9b
marioierullo <marioierullo@gmail.com>
2024-03-18 12:47:21 -0400

    cooldown for wait period for slash commands

--- events/interactionCreate.js
+++ events/interactionCreate.js
@@ -1,5 +1,4 @@
-const { Events } = require('discord.js');
-
+const { Events, Collection } = require('discord.js');
 
 /*
 You will receive an interaction for every slash command executed. 
@@ -22,7 +21,35 @@ module.exports = {
             console.error(`No command matching ${interaction.commandName} was found.`);
             return;
         }
-    
+ 
+        /*
+        You check if the cooldowns Collection already has an entry 
+        for the command being used. If this is not the case, you can add a new entry, 
+        where the value is initialized as an empty Collection. 
+        */
+        const { cooldowns } = interaction.client;
+
+        if (!cooldowns.has(command.data.name)) {
+            cooldowns.set(command.data.name, new Collection());
+        }
+        
+        const now = Date.now();
+        const timestamps = cooldowns.get(command.data.name);
+        const defaultCooldownDuration = 3;
+        const cooldownAmount = (command.cooldown ?? defaultCooldownDuration) * 1_000;
+
+        if (timestamps.has(interaction.user.id)) {
+            const expirationTime = timestamps.get(interaction.user.id) + cooldownAmount;
+        
+            if (now < expirationTime) {
+                const expiredTimestamp = Math.round(expirationTime / 1_000);
+                return interaction.reply({ content: `Please wait, you are on a cooldown for \`${command.data.name}\`. You can use it again <t:${expiredTimestamp}:R>.`, ephemeral: true });
+            }
+        }
+        
+        timestamps.set(interaction.user.id, now);
+        setTimeout(() => timestamps.delete(interaction.user.id), cooldownAmount);
+
         try {
             await command.execute(interaction);
         } catch (error) {
