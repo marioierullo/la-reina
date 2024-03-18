32985ef2a1bc001d4ba74b1de34ac24a19fa0e71
marioierullo <marioierullo@gmail.com>
2024-03-18 14:58:37 -0400

    added utility

--- commands/utility/server.js
+++ commands/utility/server.js
@@ -2,6 +2,7 @@ const { SlashCommandBuilder } = require('discord.js');
 
 module.exports = {
 	cooldown: 5,
+	category: 'utility',
 	data: new SlashCommandBuilder()
 		.setName('server')
 		.setDescription('Provides information about the server.'),
--- commands/utility/user.js
+++ commands/utility/user.js
@@ -2,6 +2,7 @@ const { SlashCommandBuilder } = require('discord.js');
 
 module.exports = {
 	cooldown: 5,
+	category: 'utility',
 	data: new SlashCommandBuilder()
 		.setName('user')
 		.setDescription('Provides information about the user.'),
